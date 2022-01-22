df7ba70175ba28294fca5edb95d43523eb786284
Chris Targett <chris@xlevus.net>
2022-01-22 20:32:11 +1300

    Refactored how some things are computed

--- src/elixir/codegen/lib/codegen/sdk/builder/dataclass.ex
+++ src/elixir/codegen/lib/codegen/sdk/builder/dataclass.ex
@@ -8,7 +8,15 @@ end
 
 defmodule Codegen.SDK.Builder.Dataclass do
   def build(name, schema, context \\ %{}) do
-    {properties, context} = extract_properties(schema, context)
+    property_spec =
+      schema
+      |> Map.get("properties", [])
+      |> Map.to_list()
+
+    {properties, context} =
+      context
+      |> Map.put_new(:root_schema, schema)
+      |> extract_properties(property_spec)
 
     klass = %Klass{
       name: name,
@@ -16,19 +24,19 @@ defmodule Codegen.SDK.Builder.Dataclass do
       docstring: Map.get(schema, "description")
     }
 
-    {klass, merge_context(context, %{classes: [klass]})}
+    {klass, update_context(context, classes: [klass])}
   end
 
-  def extract_properties(schema, context) do
-    {for {name, spec} <- schema["properties"] do
+  def extract_properties(context, properties) do
+    {for {name, spec} <- properties do
        {property, context} = process_property(name, spec, context)
        property
      end, context}
   end
 
   def process_property(name, spec, context) do
-    {type, ctx_updates} = py_type(Map.get(spec, "type"), spec, context)
-    context = merge_context(context, ctx_updates)
+    [type | ctx_updates] = py_type(Map.get(spec, "type"), spec, context)
+    context = update_context(context, ctx_updates)
 
     {
       %Property{
@@ -40,34 +48,32 @@ defmodule Codegen.SDK.Builder.Dataclass do
     }
   end
 
-  def py_type("integer", _, _), do: {"int", nil}
+  def py_type("integer", _, _), do: ["int"]
 
   def py_type("number", _, _),
-    do: {"typing.Union[int, float, decimal.Decimal]", %{imports: ["typing", "decimal"]}}
+    do: ["typing.Union[int, float, decimal.Decimal]", imports: ["typing", "decimal"]]
 
-  def py_type("string", _, _), do: {"typing.AnyStr", %{imports: ["typing"]}}
+  def py_type("string", _, _), do: ["typing.AnyStr", imports: ["typing"]]
 
-  def py_type(_, _, _), do: {"typing.Any", %{imports: ["typing"]}}
+  def py_type(_, _, _), do: ["typing.Any", imports: ["typing"]]
 
-  def merge_context(context, nil) do
+  def update_context(context, []) do
     context
   end
 
-  def merge_context(context, updates) do
-    Enum.reduce(
-      updates,
-      context,
-      fn {key, changes}, ctx -> merge_context_field(key, ctx, changes) end
-    )
+  def update_context(context, [{ctx_field, field_change} | remaining]) do
+    context
+    |> update_context_field(ctx_field, field_change)
+    |> update_context(remaining)
   end
 
-  defp merge_context_field(:imports, context, changes) do
+  defp update_context_field(context, :imports, changes) do
     Map.update(context, :imports, changes, fn val ->
       Enum.uniq(val ++ changes)
     end)
   end
 
-  defp merge_context_field(:classes, context, changes) do
+  defp update_context_field(context, :classes, changes) do
     Map.update(context, :classes, changes, fn val ->
       val ++ changes
     end)
--- src/elixir/codegen/test/codegen/sdk/builder/dataclass_test.exs
+++ src/elixir/codegen/test/codegen/sdk/builder/dataclass_test.exs
@@ -3,31 +3,32 @@ defmodule Codegen.SDK.Builder.DataclassTest do
 
   alias Codegen.SDK.Builder.Dataclass
 
+  @schema %{
+    "description" => "An example schema.",
+    "properties" => %{
+      "an_int" => %{"type" => "integer"},
+      "a_string" => %{"type" => "string"}
+    }
+  }
+
   describe "build dataclass" do
     test "builds a dataclass" do
-      {class, context} =
-        Dataclass.build("Foo", %{
-          "description" => "A Foo.",
-          "properties" => %{
-            "foo" => %{"type" => "integer"},
-            "bar" => %{}
-          }
-        })
+      {class, context} = Dataclass.build("Example", @schema)
 
-      assert class.name == "Foo"
+      assert class.name == "Example"
       assert is_list(class.properties)
-      assert class.docstring == "A Foo."
+      assert class.docstring == @schema["description"]
 
       assert context == %{
-               classes: [class]
-               # imports: ["typing"]
+               classes: [class],
+               root_schema: @schema
              }
     end
   end
 
   describe "property extraction" do
     test "extract_properties returns tuples" do
-      {property, context} =
+      {property, _} =
         Dataclass.process_property(
           "field_name",
           %{"type" => "integer", "description" => "foo"},
@@ -42,17 +43,17 @@ defmodule Codegen.SDK.Builder.DataclassTest do
 
   describe "context merging" do
     test "empty context creates imports" do
-      ctx = Dataclass.merge_context(%{}, %{imports: ["foo"]})
+      ctx = Dataclass.update_context(%{}, imports: ["foo"])
       assert ctx == %{imports: ["foo"]}
     end
 
     test "appends new imports" do
-      ctx = Dataclass.merge_context(%{imports: ["foo"]}, %{imports: ["bar"]})
+      ctx = Dataclass.update_context(%{imports: ["foo"]}, imports: ["bar"])
       assert ctx == %{imports: ["foo", "bar"]}
     end
 
     test "deduplicates new imports" do
-      ctx = Dataclass.merge_context(%{imports: ["foo"]}, %{imports: ["foo"]})
+      ctx = Dataclass.update_context(%{imports: ["foo"]}, imports: ["foo"])
       assert ctx == %{imports: ["foo"]}
     end
   end
